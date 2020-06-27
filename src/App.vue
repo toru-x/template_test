600900a6662d0396c34594973ad7fb198a61054e
toru-x <qubilaihan@gmail.com>
2020-06-27 21:01:52 +0900

    動作検証

--- src/App.vue
+++ src/App.vue
@@ -27,13 +27,25 @@
       </div>
     </form>
     <!-- <el-date-picker v-model="value6" type="date" placeholder="Pick a date" /> -->
-    <el-date-picker v-model="today" type="date" v-bind:placeholder="ph_d"  />
+    <!-- <el-date-picker v-model="today" type="date" v-bind:placeholder="ph_d"  :editable="false" :clearable="fales" :default-value="new Date()" /> -->
+    <!-- <el-date-picker v-model="today" type="date" v-init:today="ph_d" :editable="false" :clearable="fales" /> -->
+    <el-date-picker v-model="ph_d" type="date" :editable="false" :clearable="false" />
+    <!-- <el-date-picker v-model="today" type="date" v-bind:placeholder="ph_d"  v-init:today="ph_d" :editable="false"/> -->
+    <!-- <el-date-picker v-model="today" type="date" v-init:today="new Date(new Date().setFullYear(new Date().getFullYear()-26))"  :editable="false" :default-value="new Date(new Date().setFullYear(new Date().getFullYear()-26))"/> -->
+    <!-- <el-date-picker v-model="today" type="date" v-init:today="getdt"  :editable="false"/> -->
+    <!-- <el-date-picker v-model="today" type="date" /> -->
     <br>
-    <p>選択された日付：{{ today }}</p>
+    <!-- <p>選択された日付：{{ today }}</p> -->
+    <p>選択された日付：{{ ph_d }}</p>
     <!-- <el-time-picker v-model="startTime" placeholder="start time" :picker-options="{ format: 'HHmm' }"/> -->
-    <el-time-picker v-model="startTime" v-bind:placeholder="ph_t"  v-init:startTime="ph_t"/>
+    <!-- <el-time-picker v-model="startTime" v-bind:placeholder="ph_t"  v-init:startTime="ph_t"/> -->
+    <el-time-picker v-model="ph_t" :editable="false" :clearable="false"  :picker-options="{ format: 'HHmmss' }"/>
+    <el-time-picker v-model="ph_t" :editable="false" :clearable="false"  :picker-options="{ format: 'Ahhmm' }"/>
     <!-- <el-time-picker v-model="startTime"/> -->
-    <p>選択された時間：{{ startTime }}</p>
+    <!-- <p>選択された時間：{{ startTime }}</p> -->
+    <p>選択された時間：{{ ph_t }}</p>
+    <button @click="getdt">Date()関数</button>
+    <p>{{datedt}}</p>
   </div>
 </template>
 
@@ -48,10 +60,10 @@ export default {
   name:"app",
   // data(){
   data: function(){
-    var _today = new Date();
-    var twoDigit = function(value) {
-      return ('0' + value).slice(-2);
-    };
+    var _today = new Date()
+    // var twoDigit = function(value) {
+    //   return ('0' + value).slice(-2);
+    // }
     return {
       name: "kiyokiyo",
       count1: 0,
@@ -65,19 +77,18 @@ export default {
       DatePickerFormat: 'yyyy-MM-dd',
      //日本語化
       ja:ja,
-      ph_d: [
-        _today.getFullYear(),
-        twoDigit(_today.getMonth()+1),
-        twoDigit(_today.getDate())
-      ].join('-'),
-      ph_t: [
-        _today.getHours(),
-        twoDigit(_today.getMinutes()+1),
-        twoDigit(_today.getSeconds())
-      ].join(':'),
+      // ph_d: [
+      //   _today.getFullYear(),
+      //   // "2019",
+      //   twoDigit(_today.getMonth()+1),
+      //   twoDigit(_today.getDate())
+      // ].join('-'),
+      ph_d:new Date(_today.getTime() - 30 *24 * 60 * 60*1000),
+      ph_t: new Date() ,
       startTime:'',
-      today: ''
-
+      today: '',
+      datedt:'',
+      dateTime : new Date(_today.getTime() - 30*60*1000)
     }
   },
   components: {
@@ -98,6 +109,10 @@ export default {
     gettime(time){
       this.time1 =time;
       console.log('gettime')
+    },
+    getdt(){
+      // this.datedt = new Date(new Date().setFullYear(new Date().getFullYear()-26))
+      this.datedt = this.dateTime.toString()
     }
   }
 }
